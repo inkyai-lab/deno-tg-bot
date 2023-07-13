3dd58423013890685009658e5b02d7ea5ac12003
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-13 21:02:40 +0100

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -2,6 +2,23 @@ import { Bot } from "./deps.deno.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+function startMenu(ctx) {
+    ctx.replyWithHTML(`
+        <b>Welcome to GoGalaGames official AI chat support.</b>\n
+        <b>This AI CHAT is powered by openAI modal and help any crypto related issues.</b>\n\n
+        <i>All conversations are End-to-end encrypted and are fully <b>Secured & Private</b>.</i>`, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'GALA TOKEN PRICE', callback_data: 'tokenPrice' },
+                    { text: 'GET HELP', callback_data: 'support' }
+                ]
+            ]
+        }
+    })
+}
+
+// bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+    bot.command('start', ctx => { startMenu(ctx) })
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
