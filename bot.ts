f870db618878e854d30111426305a8682272b99a
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-13 21:13:48 +0100

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -19,12 +19,20 @@ function startMenu(ctx) {
 }
 
 // bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-    bot.command('start', ctx => {
-        ctx.reply(
-            "Welcome"
-        ).then(() => {
-            startMenu(ctx)
-        })
+    bot.command('start', (ctx) => {
+        ctx.replyWithHTML(`
+        <b>Welcome to GoGalaGames official AI chat support.</b>\n
+        <b>This AI CHAT is powered by openAI modal and help any crypto related issues.</b>\n\n
+        <i>All conversations are End-to-end encrypted and are fully <b>Secured & Private</b>.</i>`, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'GALA TOKEN PRICE', callback_data: 'tokenPrice' },
+                    { text: 'GET HELP', callback_data: 'support' }
+                ]
+            ]
+        }
+    })
     })
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
