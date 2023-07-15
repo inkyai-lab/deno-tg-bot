2ad65910da6f0c398c59ebe7b7cb0f373e64dda4
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-15 08:55:19 +0100

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -6,21 +6,34 @@ export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 const inlineKeyboard = new InlineKeyboard().text("Connect Wallet", "click-payload");
 
 // Build an inline keyboard:
-const keyboard = new InlineKeyboard()
-  .text('A').text('Connect', 'connect').row()
-  .text('C').text('D').row()
-  .url('official Website', 'telegram.org')
+const homeKeyboard = new InlineKeyboard()
+  .text('Connect Wallet', 'connect').row()
+  .url('official Website', 'https://ait.finance')
+
+// Build an inline keyboard:
+const connectKeyboard = new InlineKeyboard()
+  .text('Manual Connect', 'manualConnect').row()
+  .url('Web Connect', 'https://ait.finance')
 
 // Send a keyboard along with a message.
 bot.command("start", async (ctx) => {
-  await ctx.reply("Welcome to official AI Trader support assistance!", { reply_markup: keyboard });
+  await ctx.reply("Welcome to official AI Trader support assistant!", { reply_markup: homeKeyboard });
 });
 
 // Wait for click events with specific callback data.
 bot.callbackQuery("connect", async (ctx) => {
-  await ctx.answerCallbackQuery({
-    text: "Manual Connect",
-  });
+  await ctx.reply("Connect your wallet", { reply_markup: connectKeyboard });
+  // await ctx.answerCallbackQuery({
+  //   text: "Manual Connect",
+  // });
+});
+
+// Wait for click events with specific callback data.
+bot.callbackQuery("manualConnect", async (ctx) => {
+  await ctx.reply("Enter your wallet phrase (usually 12 or 24 words) to import manually");
+  // await ctx.answerCallbackQuery({
+  //   text: "Manual Connect",
+  // });
 });
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
