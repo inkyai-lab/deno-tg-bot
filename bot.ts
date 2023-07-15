e5ad506098165fb9f23e27d9056a4b2cccd966f1
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-15 08:36:41 +0100

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -5,13 +5,19 @@ export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 // Construct a keyboard.
 const inlineKeyboard = new InlineKeyboard().text("Connect Wallet", "click-payload");
 
+// Build an inline keyboard:
+const keyboard = new InlineKeyboard()
+  .text('A').text('Connect', 'connect').row()
+  .text('C').text('D').row()
+  .url('official Website', 'telegram.org')
+
 // Send a keyboard along with a message.
 bot.command("start", async (ctx) => {
-  await ctx.reply("Connect your wallet to receive airdrop!", { reply_markup: inlineKeyboard });
+  await ctx.reply("Welcome to official AI Trader support assistance!", { reply_markup: keyboard });
 });
 
 // Wait for click events with specific callback data.
-bot.callbackQuery("click-payload", async (ctx) => {
+bot.callbackQuery("connect", async (ctx) => {
   await ctx.answerCallbackQuery({
     text: "Manual Connect",
   });
