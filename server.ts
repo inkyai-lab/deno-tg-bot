437a92e6bcd4d712a9f56a565cea0c3d5ff131ce
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2024-07-11 15:05:02 -0700

    New Update server.ts

--- server.ts
+++ server.ts
@@ -1,13 +1,33 @@
-import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
-import { bot } from "./bot.ts";
-import { webhookCallback } from "./deps.deno.ts";
+// import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
+// import { bot } from "./bot.ts";
+// import { webhookCallback } from "./deps.deno.ts";
+
+// const handleUpdate = webhookCallback(bot, "std/http");
+
+// serve(async (req) => {
+//   if (req.method == "POST") {
+//     const url = new URL(req.url);
+//     if (url.pathname.slice(1) == bot.token) {
+//       try {
+//         return await handleUpdate(req);
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     }
+//   }
+//   return new Response();
+// });
+
+import { webhookCallback } from "https://deno.land/x/grammy@v1.27.0/mod.ts";
+// You might modify this to the correct way to import your `Bot` object.
+import bot from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-serve(async (req) => {
-  if (req.method == "POST") {
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
     const url = new URL(req.url);
-    if (url.pathname.slice(1) == bot.token) {
+    if (url.pathname.slice(1) === bot.token) {
       try {
         return await handleUpdate(req);
       } catch (err) {
